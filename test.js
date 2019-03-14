583d3c877de80c2a922ec1edc6e942b81660db9d
Ania Blaziak <anblaziak@gmail.com>
2019-03-14 11:11:57 +0000

    Completed and tested addTodo function

--- test.js
+++ test.js
@@ -1,24 +1,71 @@
 var test = require('tape');
 var logic = require('./logic');
 
-test('Example', function (t) {
-    t.pass();
-    t.end();
-});
+var exampleArray = [{
+    id: 0,
+    description: 'smash avocados',
+    done: true,
+  },
+  {
+    id: 1,
+    description: 'make coffee',
+    done: false,
+  },
+];
 
-test("To-do output", (t) => {
-  const actual = logic.addTodo([1, 2, 3, 4], 1);
-  const expected = [ 1, 2, 3, 4, 1];
-  t.deepEqual(actual, expected, "Input [1] should return [1, 2]");
+var newArray = [{
+  id: 0,
+  description: 'make smoothie out of things that should really be cooked',
+  done: false,
+}];
+
+
+test('Example', function (t) {
+  t.pass();
   t.end();
 });
 
-// test("To-do output", (t) => {
-//   const actual = logic.deleteTodo();
-//   const expected = 1;
-//   t.equal(actual, expected, "Input 1 should return 1");
-//   t.end();
-// });
+test("To-do output", function (t) {
+  const testArray = logic.addTodo(exampleArray, "avocado");
+  const actual = Array.isArray(testArray);
+  const expected = true;
+  t.deepEqual(actual, expected, "Output should be an array");
+  t.end();
+})
+
+test("To-do output", function (t) {
+  const actual = exampleArray;
+  const expected = exampleArray.slice();
+  logic.addTodo(exampleArray, newArray);
+  t.deepEqual(actual, expected, "Test old array doesn't change");
+  t.end();
+})
+
+test("To-do output", function (t) {
+  const testArray = logic.addTodo(exampleArray, "avocado");
+  const actual = testArray[testArray.length - 1].description;
+  const expected = "avocado";
+  t.deepEqual(actual, expected, "Output should be avocado")
+  t.end();
+});
+
+test("To-do output", function (t) {
+  const testArray = logic.addTodo(exampleArray, "avocado");
+  const actual = testArray[testArray.length - 1].done;
+  const expected = false;
+  t.deepEqual(actual, expected, "Done should be false")
+  t.end();
+});
+
+test("To-do output", (t) => {
+  const actual = logic.deleteTodo([{
+    id: 1,
+    description: 2
+  }], 1);
+  const expected = [];
+  t.deepEqual(actual, expected, "Input 1 should return []");
+  t.end();
+});
 //
 // test("To-do output", (t) => {
 //   const actual = logic.markTodo();
